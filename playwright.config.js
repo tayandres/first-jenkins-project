734e979febfbc1d3221f974ecf75bd548b178bb8
tayandres <tayloralexisandres@gmail.com>
2024-10-05 12:56:51 -0600

    update package.json

--- playwright.config.js
+++ playwright.config.js
@@ -13,13 +13,14 @@ const { defineConfig, devices } = require('@playwright/test');
 module.exports = defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: 1,
+  /*process.env.CI ? 1 : undefined,*/
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -29,15 +30,19 @@ module.exports = defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    headless: false,
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: { 
+        ...devices['Desktop Chrome'],
+        viewport: {width: 1850, height: 1000}
+       },
     },
-
+/*
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
@@ -47,16 +52,16 @@ module.exports = defineConfig({
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
+*/
+    
+   //  {
+     //  name: 'Mobile Chrome',
     //   use: { ...devices['Pixel 5'] },
-    // },
+ //    },
     // {
-    //   name: 'Mobile Safari',
+     //  name: 'Mobile Safari',
     //   use: { ...devices['iPhone 12'] },
-    // },
+  //   },
 
     /* Test against branded browsers. */
     // {
